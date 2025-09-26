a6a28f95f4cee4a073ff53d6fbc041d6b0a4303b
jay <wjay.inclino@gmail.com>
2025-09-26 11:40:03 +0800

    update routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(1000);
+        });
+
+        // all routes with {id} parameter must be numeric
+        Route::pattern('id', '[0-9]+');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,30 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+| Should always use named routes for easier maintenance.
+| Add Rate limiters to routes as needed.
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+*/
+
+Route::middleware(['throttle:global'])->group(function () {
+    // Place all routes that need global rate limiting here
+
+    Route::get('/', function () {
+        return Inertia::render('Welcome');
+    })->name('home');
+    
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
