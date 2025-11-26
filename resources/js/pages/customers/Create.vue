3fe74ae4e161581c79e588e0bd96f1c8c3dbe5b9
jay <wjay.inclino@gmail.com>
2025-11-26 10:56:42 +0800

    update create customer

--- resources/js/pages/customers/Create.vue
+++ resources/js/pages/customers/Create.vue
@@ -84,7 +84,7 @@ async function submit() {
                                 v-model="form.starting_balance"
                                 step="0.01"
                                 min="0"
-                                placeholder="0.00`"
+                                placeholder="0.00"
                                 class="dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100"
                             />
                             <InputError :message="form.errors.starting_balance" class="mt-1" />
