888e32972823d749d388bf4e9b57e221f2b63bec
jay <wjay.inclino@gmail.com>
2025-12-03 15:26:42 +0800

    disable ssr inertia

--- config/inertia.php
+++ config/inertia.php
@@ -16,7 +16,7 @@ return [
     */
 
     'ssr' => [
-        'enabled' => true,
+        'enabled' => false, // Temporarily disabled to test if SSR is causing 502 errors
         'url' => 'http://127.0.0.1:13714',
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
     ],
